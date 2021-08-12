028d1a3037d743a9eab52ee428a4ef7abca1236e
UrmilaVadi <gopi.ghadiya@gmail.com>
2021-08-12 22:54:32 +0300

    Final commit

--- cypress/integration/TestCases/Site1.spec.js
+++ cypress/integration/TestCases/Site1.spec.js
@@ -1,17 +1,23 @@
-import ElementObjects from '../pageObjects/elementObjects'
+import Homepage from '../pageObjects/homepage'
 import Logo from '../pageObjects/logo'
+import Formpage from '../pageObjects/formpage'
+import Errorpage from '../pageObjects/errorpage'
+
+
 
 describe('Site 1 ("http://172.19.192.1:8080/") - All Test Cases',function(){
 
-    const elementObjects  = new ElementObjects()
-    const logo  = new Logo()
+    const homepage  = new Homepage()
+    const logo = new Logo()
+    const formpage = new Formpage()
+    const errorpage = new Errorpage()
 
     beforeEach(function(){
         cy.visit('http://172.19.192.1:8080/')
     })
 
     it('The Title should be "UI Testing Site"',function(){
-        elementObjects.getTitle().should('include','UI Testing Site')  //UI testing site tite contains     
+        homepage.getTitle().should('include','UI Testing Site')  //UI testing site tite contains     
     })
 
     it('The Company Logo should be visible ',function(){
@@ -20,83 +26,78 @@ describe('Site 1 ("http://172.19.192.1:8080/") - All Test Cases',function(){
     })
 
     it('Click on Home button that should be navigate to Home page  ',function(){
-        elementObjects.home().should('be.visible')           //Home button should be visible
-        elementObjects.home().contains('Home').click()       // To click Home button
+        homepage.home().should('be.visible')           //Home button should be visible
+        homepage.home().contains('Home').click()       // To click Home button
     })
 
     it('Click on Home button it should turn active status',function(){
-        elementObjects.home().contains('Home').click()
+        homepage.home().contains('Home').click()
         // active class validate display
-        elementObjects.active().click({ force: true })  
+        homepage.active().click({ force: true })  
         // After click Home button "Home" text color should be white
-        elementObjects.home().should('have.css', 'color', 'rgb(255, 255, 255)').should('not.have.css','color','rgb(157,157,157)')      
+        homepage.home().should('have.css', 'color', 'rgb(255, 255, 255)').should('not.have.css','color','rgb(157,157,157)')      
     })
 
     it('Click on Form button that should be navigate to Form Page ',function(){
-        elementObjects.form().contains('Form').click()       // To click Form button
+        formpage.form().contains('Form').click()       // To click Form button
     })
 
     it('Click on Form button of it should turn active status',function(){
-        elementObjects.form().contains('Form').click()
+        formpage.form().contains('Form').click()
         // active class validate display
-        elementObjects.active().click({ force: true }) 
+        formpage.active().click({ force: true }) 
         // After click Form button "Form" text color should be white
-        elementObjects.form().should('have.css', 'color', 'rgb(255, 255, 255)').should('not.have.css','color','rgb(157,157,157)')      
+        formpage.form().should('have.css', 'color', 'rgb(255, 255, 255)').should('not.have.css','color','rgb(157,157,157)')      
     })
 
     it('Click on Error button, Http code 404 should be display',function(){
-        elementObjects.error().contains('Error').click()       // To click Error button
-        elementObjects.errorPageText()  //Validate 404 Error text contains  
+        errorpage.error().contains('Error').click()       // To click Error button
+        errorpage.errorPageText()  //Validate 404 Error text contains  
     })
 
     it('Click on UI Testing button , thats navigate to Home Page',function(){
-        elementObjects.uiTesting().contains('UI Testing').click()       // To click UI Testing button
+        homepage.uiTesting().contains('UI Testing').click()       // To click UI Testing button
         // After click UI Testing button "Home" Page should be display and Home text should be highlight
-        elementObjects.homePageText().should('be.visible')
-        elementObjects.home().should('have.css', 'color', 'rgb(255, 255, 255)').should('not.have.css','color','rgb(157,157,157)')
+        homepage.homePageText().should('be.visible')
+        homepage.home().should('have.css', 'color', 'rgb(255, 255, 255)').should('not.have.css','color','rgb(157,157,157)')
     })
 
     it('Click on Home button,in Home Page h1 tag "Welcome to Pixelmatic QA department" text should be display',function(){
-        elementObjects.home().contains('Home').click()       // To click Home button
-        elementObjects.homePageText()  // Check Welcome to Pixelmatic QA department text display
+        homepage.home().contains('Home').click()       // To click Home button
+        homepage.homePageText()  // Check Welcome to Pixelmatic QA department text display
     })
 
     it('Click on Home button, in Home Page p tag "This site is dedicated to perform..text display" ',function(){
-        elementObjects.home().contains('Home').click()       // To click Home button
-        elementObjects.homePageTextP()  // Check text This site is dedicated to perform some exercises and demonstrate
+        homepage.home().contains('Home').click()       // To click Home button
+        homepage.homePageTextP()  // Check text This site is dedicated to perform some exercises and demonstrate
     })
 
     it('Form Page input box and submit button should display',function(){
-        elementObjects.form().contains('Form').click()       // To click Form button
-        elementObjects.formPageText()  // To check Simple Form submission text display
-        elementObjects.forminput().should('be.visible').should('be.enabled') //To check input box visible and enable
-        elementObjects.formsubmit().should('be.visible').should('be.enabled') //To check Submit button visible and enable
+        formpage.form().contains('Form').click()       // To click Form button
+        formpage.formPageText()  // To check Simple Form submission text display
+        formpage.forminput().should('be.visible').should('be.enabled') //To check input box visible and enable
+        formpage.formsubmit().should('be.visible').should('be.enabled') //To check Submit button visible and enable
     })
 
     it('In Form Page after Enter Value in input box and submit, Hello Page should be display',function(){
-        elementObjects.form().contains('Form').click()       // To click Form button
-        elementObjects.formPageText()  // To check Simple Form submission text display
-        elementObjects.forminput().should('be.visible').should('be.enabled').type('John') //To check input box visible and enable
-        elementObjects.formsubmit().should('be.visible').should('be.enabled').click() //To check Submit button visible and enable
-        elementObjects.helloPagetext().contains('Hello John!')
+        formpage.form().contains('Form').click()       // To click Form button
+        formpage.formPageText()  // To check Simple Form submission text display
+        formpage.forminput().should('be.visible').should('be.enabled').type('John') //To check input box visible and enable
+        formpage.formsubmit().should('be.visible').should('be.enabled').click() //To check Submit button visible and enable
+        formpage.helloPagetext().contains('Hello John!')
         cy.go(-1)
-        elementObjects.forminput().clear().type('Sophia')
-        elementObjects.formsubmit().click()
-        elementObjects.helloPagetext().contains('Hello Sophia!')
+        formpage.forminput().clear().type('Sophia')
+        formpage.formsubmit().click()
+        formpage.helloPagetext().contains('Hello Sophia!')
         cy.go(-1)
-        elementObjects.forminput().clear().type('Charlie')
-        elementObjects.formsubmit().click()
-        elementObjects.helloPagetext().contains('Hello Charlie!')
+        formpage.forminput().clear().type('Charlie')
+        formpage.formsubmit().click()
+        formpage.helloPagetext().contains('Hello Charlie!')
         cy.go(-1)
-        elementObjects.forminput().clear().type('Emily')
-        elementObjects.formsubmit().click()
-        elementObjects.helloPagetext().contains('Hello Emily!')
+        formpage.forminput().clear().type('Emily')
+        formpage.formsubmit().click()
+        formpage.helloPagetext().contains('Hello Emily!')
 
     })
-
-
-
-
-
 
 })
